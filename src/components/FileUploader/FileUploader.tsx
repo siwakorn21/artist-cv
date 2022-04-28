128fb3667882b622eec1391af25e41e25044665c
siwakorn21 <siwakornboonmun@gmail.com>
2022-04-29 00:30:53 +0700

    update code

--- src/components/FileUploader/FileUploader.tsx
+++ src/components/FileUploader/FileUploader.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface FileUploadProps {
     file?: File;
@@ -7,18 +7,45 @@ interface FileUploadProps {
 }
 
 const FileUploaderContainer = styled.div`
-    width: 100%;
-    height: 100%;
-    border: 1px dashed black;
-    border-radius: 1rem;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+    .image-container {
+        width: 20rem;
+        height: 20rem;
+        border: 1px dashed black;
+        border-radius: 1rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    input {
+        width: 60%;
+        margin: 1rem auto;
+    }
 `;
 
 const FileUploader: React.FC<FileUploadProps> = ({ file, onChange }) => {
+    const [objectUrl, setObjectUrl] = useState<string | undefined>(undefined);
+
+    useEffect(() => {
+        if (file) {
+            setObjectUrl(URL.createObjectURL(file));
+        }
+    }, [file]);
+
     return (
         <FileUploaderContainer>
+            <div className="image-container">
+                <img src={objectUrl} />
+            </div>
             <input type="file" onChange={onChange} />
         </FileUploaderContainer>
     );
